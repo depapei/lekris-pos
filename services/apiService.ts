3b0600ff8c47cca6fa45a28c57268baca8ae0c70
RANGGA ANDIKA WIBISANA <ranggaa606@gmail.com>
2026-02-22 20:21:39 +0700

    adjust apiService base url to api-mini-pos-daltek

--- services/apiService.ts
+++ services/apiService.ts
@@ -3,7 +3,7 @@ import { Product, Supplier, Transaction } from "../types";
 /**
  * REST API SERVICE
  */
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://Api-mini-pos.daltek.id/api";
 
 const request = async (path: string, options: RequestInit = {}) => {
   try {
