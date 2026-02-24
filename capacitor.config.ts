3e820971e362150537d02e6c3a02595aee8cc1d5
RANGGA ANDIKA WIBISANA <ranggaa606@gmail.com>
2026-02-24 17:09:59 +0700

    change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.lekris.pos',
-  appName: 'lele-krispy-pos',
-  webDir: 'dist'
+  appId: "com.lekris.pos",
+  appName: "Lele Krispy",
+  webDir: "dist",
 };
 
 export default config;
